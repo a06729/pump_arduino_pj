2526e1618267f0807acec4a3f4f6b7d1505c016d
MongRaGong <33287431+a06729@users.noreply.github.com>
2025-11-21 10:50:50 +0900

    모터2 275,200,100ml 보정값 적용
    두번째 모터 275,200,100ml 보정값 적용 및 테스트 완료

--- freeRtos_uart/motor.c
+++ freeRtos_uart/motor.c
@@ -12,6 +12,12 @@
 #define MIN_FLOW_TIME_MS 80      // 최소 구동 시간 80ms
 #define TIME_OFFSET_S 0.05       // 가속 보상 시간 0.05초
 
+
+#define PUMP_FLOW_RATE_S_M2 1.667 // 100 ml/min을 초 단위로 변환한 값 (ml/s)
+#define MIN_FLOW_TIME_MS_M2 80      // 최소 구동 시간 80ms
+#define TIME_OFFSET_S_M2 0.05       // 가속 보상 시간 0.05초
+
+
 //공식 100/28047ms
 //28047ms 모터의 유량 흐름 값
 //3.22
@@ -62,9 +68,31 @@ void motor_W1(int32_t data){
 //모터2 동작시키는 기능
 void motor_W2(int32_t data){
 	
-	int32_t target = data;
-	double flow_time = (target/Pump_Flow_Second) * 1000;
+	double target_d = (double)data;
 	
+	double C=0; // 보정 계수 변수 선언
+	
+	if (data <= 150) {
+		C = 0.603; // 작은 양에 대해 이전 보정 계수 사용
+	}else if(data<=200){
+		//이전값:0.643
+		C= 0.633;
+	}else {
+		//0.700 이전값 (275ml를 넣었을시 정확하게 나온다)
+		C = 0.660; // 큰 양에 대해 새로운 보정 계수 사용 (예시 값)
+	}
+	
+	// 2. 보정된 시간 계산 (단위: 초)
+	// Time(s) = (Target / Flow_Rate + T_offset) * C
+	double time_in_seconds = (target_d / PUMP_FLOW_RATE_S_M2 + TIME_OFFSET_S_M2) * C;
+	
+	// 3. 밀리초(ms)로 변환
+	int32_t flow_time = (int32_t)(time_in_seconds * 1000.0);
+
+	// 4. 최소 시간 보장
+	if (flow_time < MIN_FLOW_TIME_MS_M2) {
+		flow_time = MIN_FLOW_TIME_MS_M2;
+	}
 	
 	PORTB |= (1<<PB1);
 	delay(flow_time);
